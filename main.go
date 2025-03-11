b395e5aaa916041626765a655eb369d32126415b
puneetjindal1997 <puneetjindal1997@gmail.com>
2025-03-11 14:59:24 +0530

    kafka refined producers

--- main.go
+++ main.go
@@ -3,65 +3,54 @@ package main
 import (
 	"context"
 	"fmt"
+	"strconv"
+	"strings"
 	"sync"
 
 	"github.com/twmb/franz-go/pkg/kadm"
 	"github.com/twmb/franz-go/pkg/kgo"
 )
 
+const topicName string = "yt-kafka-lecture"
+
 var brokers = []string{"localhost:29092", "localhost:29093", "localhost:29094"}
 
-var adminClient *kadm.Client
-
-func getAdminClient() {
-
+func getAdminClient() *kgo.Client {
+	balancer := kgo.RoundRobinBalancer()
 	client, err := kgo.NewClient(
 		kgo.SeedBrokers(brokers...),
+		kgo.Balancers(balancer),
 	)
 	if err != nil {
 		panic(err)
 	}
 
-	adminClient = kadm.NewClient(client)
-
-}
-
-func getSimpleKafkaClient() *kgo.Client {
-	client, err := kgo.NewClient(
-		kgo.SeedBrokers(brokers...),
-	)
+	adminClient := kadm.NewClient(client)
+	_, err = adminClient.CreateTopic(context.Background(), 10, -1, nil, topicName)
 	if err != nil {
-		panic(err)
+		if !strings.Contains(err.Error(), "TOPIC_ALREADY_EXISTS") {
+			panic(err)
+		}
 	}
-
 	return client
 }
 
 func main() {
-	// topic defination
-	topicName := "yt-kafka-lecture"
-
-	//init admin client
-	getAdminClient()
-
-	// config
-	// retention time use
-	// properties set
-
-	// creation of topic
-	creationResp, err := adminClient.CreateTopic(context.Background(), 10, -1, nil, topicName)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(creationResp)
-
-	// Simple producer client init
-	simpleClient := getSimpleKafkaClient()
+	simpleClient := getAdminClient()
 
 	var wg sync.WaitGroup
+
+	for i := 0; i < 100; i++ {
+		KafkaProducer(i, &wg, simpleClient)
+	}
+	wg.Wait()
+}
+
+func KafkaProducer(i int, wg *sync.WaitGroup, simpleClient *kgo.Client) {
+	kafkaKey := strconv.Itoa(i)
 	wg.Add(1)
 	// prepare record to produce over kafka
-	record := &kgo.Record{Topic: topicName, Value: []byte("Our first hello to kafka")}
+	record := &kgo.Record{Topic: topicName, Key: []byte("kafka_" + kafkaKey), Value: []byte(fmt.Sprintf("Our %s hello to kafka", kafkaKey))}
 	// produce
 	simpleClient.Produce(context.Background(), record, func(_ *kgo.Record, err error) {
 		defer wg.Done()
@@ -70,7 +59,6 @@ func main() {
 		}
 
 	})
-	wg.Wait()
 }
 
 func kafkaProducerClientClose(client *kadm.Client) {
